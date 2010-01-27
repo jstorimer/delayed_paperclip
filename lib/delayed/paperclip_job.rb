6712736b2e63c596d14550d451d2cc976f9b4fe0
Jesse Storimer <jesse@jadedpixel.com>
2010-01-27 08:24:14 -0500

    Removed processing attribute.

--- lib/delayed/paperclip_job.rb
+++ lib/delayed/paperclip_job.rb
@@ -2,11 +2,6 @@ class DelayedPaperclipJob < Struct.new(:instance_id, :instance_klass, :attachmen
   def perform
     instance = instance_klass.constantize.find(instance_id)
 
-    instance.send(attachment_name).reprocess!
-    
-    if instance.respond_to?(:processing)
-      instance.processing = false 
-      instance.save
-    end
+    instance.send(attachment_name).reprocess!    
   end
 end
--- test/delayed_paperclip_test.rb
+++ test/delayed_paperclip_test.rb
@@ -29,22 +29,6 @@ class DelayedPaperclipTest < Test::Unit::TestCase
     assert !@dummy.halt_processing_for_image
   end
   
-  def test_halt_processing_if_source_changed_and_processing_attribute_defined
-    reset_table("dummies") do |d|
-      d.string :image_file_name
-      d.integer :image_file_size
-      d.boolean :processing
-    end
-    @dummy_class = reset_class "Dummy"
-    @dummy_class.has_attached_file :image
-    @dummy_class.process_in_background :image
-    @dummy = Dummy.new(:image => File.read("#{RAILS_ROOT}/test/fixtures/12k.png"))
-    
-    @dummy.stubs(:image_changed?).returns(true)
-    assert !@dummy.halt_processing_for_image
-    assert @dummy.processing
-  end
-  
   def test_halt_processing_if_source_has_not_changed
     @dummy.stubs(:image_changed?).returns(false)
     assert_not_equal false, @dummy.halt_processing_for_image
@@ -69,27 +53,6 @@ class DelayedPaperclipTest < Test::Unit::TestCase
     
     @dummy.save!
     DelayedPaperclipJob.new(@dummy.id, @dummy.class.name, :image).perform
-  end
-  
-  def test_perform_job_with_processing_attribute
-    reset_table("dummies") do |d|
-      d.string :image_file_name
-      d.integer :image_file_size
-      d.boolean :processing
-    end
-    @dummy_class = reset_class "Dummy"
-    @dummy_class.has_attached_file :image
-    @dummy_class.process_in_background :image
-    
-    @dummy = Dummy.new(:image => File.open("#{RAILS_ROOT}/test/fixtures/12k.png"))
-
-    Paperclip::Attachment.any_instance.expects(:reprocess!)
-    
-    @dummy.processing = true
-    @dummy.save!
-    DelayedPaperclipJob.new(@dummy.id, @dummy.class.name, :image).perform
-    
-    assert !@dummy.reload.processing
   end
   
   private 
