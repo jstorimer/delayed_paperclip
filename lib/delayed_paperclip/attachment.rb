8ab234feba486d19427e856f40096c054d56c3e1
Tom Meier <tom@venombytes.com>
2012-03-20 16:46:49 +1100

    The real fix - The underlying attached image was getting out of sync. Paperclip reprocess method updates the instance updated_at but not the image_updated_at, this means the URLs get out of sync if the queue takes too long to pick up the item (or process takes > 1 second)

--- lib/delayed_paperclip/attachment.rb
+++ lib/delayed_paperclip/attachment.rb
@@ -45,12 +45,10 @@ module DelayedPaperclip
       def post_process_styles_with_processing(*args)
         post_process_styles_without_processing(*args)
 
-        # update_column is available in rails 3.1 instead we can do this to update the attribute without callbacks
-
-        #instance.update_column("#{name}_processing", false) if instance.respond_to?(:"#{name}_processing?")
         if instance.respond_to?(:"#{name}_processing?")
           instance.send("#{name}_processing=", false)
-          instance.class.update_all({ "#{name}_processing" => false }, instance.class.primary_key => instance.id)
+
+          instance.class.update_all({ "#{name}_processing" => false, "#{name}_updated_at" => Time.at(self.updated_at) }, instance.class.primary_key => instance.id)
         end
       end
 
